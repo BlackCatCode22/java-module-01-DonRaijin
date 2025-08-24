840af2289d10356dcbd62ad4e7dda96480690a09
DonRaijin <donbounleuang@gmail.com>
2025-08-24 16:46:25 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+// HellWorld.java
+// db 8/24/25
+
+// Description of program
+// This program will say Hello World
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello World and Welcome!");
+    }
+}
